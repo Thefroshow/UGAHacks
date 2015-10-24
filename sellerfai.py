4b2d1c5a192aac54352a490b75be9cb7ec76bbaf
rnsaney <rnsaney@uga.edu>
2015-10-23 21:27:51 -0400

    Update sellerfai.py

--- sellerfai.py
+++ sellerfai.py
@@ -9,6 +9,6 @@ from clarifai.client import ClarifaiApi
 import urllib2
 
 endpoint = "http://research.api.sellerlabs.com/"
-key = 1445656217|slapi|c546db3b045236ccad072dea3d86317b
+key = "1445656217|slapi|c546db3b045236ccad072dea3d86317b"
 response = urllib2.urlopen('http://python.org/')
 html = response.read()
