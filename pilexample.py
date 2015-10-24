ad340286610736834912b63b1eb9771598ff9e06
Joshua <codeathens@gmail.com>
2015-10-24 17:03:03 -0400

    Pillow Example Image Filtering

--- pilexample.py
+++ pilexample.py
@@ -1,8 +1,29 @@
 from PIL import Image, ImageFilter, ImageShow
 import urllib, cStringIO
+testimgURL = "http://www.man7.org/tlpi/cover/TLPI-front-cover.png"
+testFile = cStringIO.StringIO(urllib.urlopen(testimgURL).read())
 
-file = cStringIO.StringIO(urllib.urlopen("https://upload.wikimedia.org/wikipedia/en/4/4f/Sharpe's_Siege_front_book_cover.jpg").read())
-im = Image.open(file)
-im.save("file_clean.jpg")
-imblur = im.filter(ImageFilter.BLUR)
-imblur.save("file_blurred.jpg")
+
+def filterFun(file):
+    """FUN WITH PIL.ImageFilter!"""
+
+    im = Image.open(file)
+    im.save("ImageTesting/file_clean.png")
+
+    #blur dat shit
+    imBlur = im.filter(ImageFilter.BLUR)
+    imBlur.save("ImageTesting/file_blurred.png")
+
+    #smooth dat shit
+    imSmooth = im.filter(ImageFilter.SMOOTH)
+    imSmooth.save("ImageTesting/file_smooth.png")
+
+    #SHARPEN
+    imSharpen = im.filter(ImageFilter.SHARPEN)
+    imSharpen.save("ImageTesting/file_sharpen.png")
+
+    #DETAIL
+    imDetail = im.filter(ImageFilter.DETAIL)
+    imDetail.save("ImageTesting/file_detail.png")
+
+filterFun(testFile)
