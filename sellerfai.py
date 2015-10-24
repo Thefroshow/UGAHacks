54b8ec7987b5dce0cc4ca97a7d7aa5491afeef8c
rnsaney <rnsaney@uga.edu>
2015-10-23 20:49:59 -0400

    Update sellerfai.py

--- sellerfai.py
+++ sellerfai.py
@@ -1,4 +1,13 @@
+#Richard Saney
+#Josh Harper
+#Alex Hunt
+
+#[endpoint] => http://research.api.sellerlabs.com/
+#[key] => 1445656217|slapi|c546db3b045236ccad072dea3d86317b
 
 import urllib2
+
+endpoint = "http://research.api.sellerlabs.com/"
+key = 1445656217|slapi|c546db3b045236ccad072dea3d86317b
 response = urllib2.urlopen('http://python.org/')
 html = response.read()
