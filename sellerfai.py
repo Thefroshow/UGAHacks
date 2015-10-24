c4ff8b6b885851b7055d897a4ca97ac7b947b689
rnsaney <rnsaney@uga.edu>
2015-10-23 21:24:10 -0400

    Update sellerfai.py

--- sellerfai.py
+++ sellerfai.py
@@ -5,6 +5,7 @@
 #[endpoint] => http://research.api.sellerlabs.com/
 #[key] => 1445656217|slapi|c546db3b045236ccad072dea3d86317b
 
+from clarifai.client import ClarifaiApi
 import urllib2
 
 endpoint = "http://research.api.sellerlabs.com/"
