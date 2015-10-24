ba1badc53facecb07526b0c98d133b9d95fe7647
Richard Saney <rnsaney@uga.edu>
2015-10-23 22:52:36 -0400

    updated sellerfai.py

--- sellerfai.py
+++ sellerfai.py
@@ -5,10 +5,17 @@
 #[endpoint] => http://research.api.sellerlabs.com/
 #[key] => 1445656217|slapi|c546db3b045236ccad072dea3d86317b
 
+from types import StringTypes
+from clarifai_basic import ClarifaiCustomModel
 from clarifai.client import ClarifaiApi
 import urllib2
 
+#image to tags
+
+#tags dictionary
+
+
 endpoint = "http://research.api.sellerlabs.com/"
 key = "1445656217|slapi|c546db3b045236ccad072dea3d86317b"
-response = urllib2.urlopen('http://python.org/')
+response = urllib2.urlopen('http://')
 html = response.read()
