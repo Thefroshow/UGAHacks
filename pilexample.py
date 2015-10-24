02e9fe17f97f50a6168da9220f22ed3e7b5d9944
Joshua <codeathens@gmail.com>
2015-10-24 16:40:25 -0400

    blurs image, saves image

--- pilexample.py
+++ pilexample.py
@@ -1,6 +1,8 @@
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageShow
 import urllib, cStringIO
 
-file = cStringIO.StringIO(urllib.urlopen("http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg").read())
+file = cStringIO.StringIO(urllib.urlopen("https://upload.wikimedia.org/wikipedia/en/4/4f/Sharpe's_Siege_front_book_cover.jpg").read())
 im = Image.open(file)
+im.save("file_clean.jpg")
 imblur = im.filter(ImageFilter.BLUR)
+imblur.save("file_blurred.jpg")
