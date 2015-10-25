7ff3abc6c37dc6d0793c6d69c97ba0e91e6e0f48
Alexander Jaris Hunt <Jaris@Alexanders-MacBook-Pro.local>
2015-10-25 06:47:02 -0400

    Moved files around and got rid of backups

--- test.py
+++
@@ -1,6 +0,0 @@
-from clarifai.client import ClarifaiApi
-clarifai_api = ClarifaiApi() # assumes environment variables are set.
-result = clarifai_api.tag_images(open('/Users/Jaris/Desktop/Lighthouse.jpg'))
-
-print result
-
